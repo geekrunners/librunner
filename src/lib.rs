c64e53411e35d67cd053f3ad3b9df6dc7db9abcd
Hildeberto Mendonca <me@hildeberto.com>
2023-07-04 15:19:57 -0400

    Moved features from Race to Running

--- src/lib.rs
+++ src/lib.rs
@@ -197,7 +197,7 @@ pub mod utils {
 
 /// API to make running calculations.
 pub mod running {
-    use std::time::Duration;
+    use std::{time::Duration, any::Any};
 
     /// A running race, already with common calculations that work with multiple scales.
     pub trait Race {
@@ -265,6 +265,8 @@ pub mod running {
 
         /// Returns the duration of the race.
         fn duration(&self) -> Duration;
+
+        fn as_any(&self) -> &dyn Any;
         
         /// Calculates the average pace based on distance and duration.
         /// 
@@ -351,71 +353,6 @@ pub mod running {
 
             splits
         }
-
-        /// Returns the splits of the race from a higher to a lower pace, according to the degree of variation.
-        /// 
-        /// # Arguments
-        /// 
-        /// * `degree` - the degree of variation from the average pace in seconds.
-        fn negative_splits(&self, degree: Duration) -> Vec<Duration> {
-            // minutes between minimal and maximum pace
-            let variation = (2 * degree.as_secs()) + 1;
-            let num_splits = self.num_splits();
-            // size of the block of splits with the same pace
-            let block = num_splits / variation;
-            let average_pace = self.average_pace();
-
-            let mut negative_splits = Vec::new();
-            // the pace starts high and decrements at every splits block
-            let mut pace = Duration::new(average_pace.as_secs() + degree.as_secs(), 0);
-            let mut block_count = 0;
-            
-            for _n in 0..num_splits as usize {
-                if block == block_count {
-                    // decrements the pace at every new block.
-                    let secs = pace.as_secs() - 1u64;
-                    pace = Duration::new(secs, 0);
-
-                    block_count = 0;
-                }
-                negative_splits.push(pace);
-                block_count += 1;
-            }
-
-            negative_splits
-        }
-
-        /// Returns the splits of the race from a lower to a higher pace, according to the degree of variation.
-        /// 
-        /// # Arguments
-        /// 
-        /// * `degree` - the degree of variation from the average pace in seconds.
-        fn positive_splits(&self, degree: Duration) -> Vec<Duration> {
-            let variation = (2 * degree.as_secs()) + 1;
-            let num_splits = self.num_splits();
-            // size of the block of splits with the same pace
-            let block = num_splits / variation;
-            let average_pace = self.average_pace();
-
-            let mut positive_splits = Vec::new();
-            // the pace starts high and decrements at every splits block
-            let mut pace = Duration::new(average_pace.as_secs() - degree.as_secs(), 0);
-            let mut block_count = 0;
-            
-            for _n in 0..num_splits as usize {
-                if block == block_count {
-                    // decrements the pace at every new block.
-                    let secs = pace.as_secs() + 1u64;
-                    pace = Duration::new(secs, 0);
-
-                    block_count = 0;
-                }
-                positive_splits.push(pace);
-                block_count += 1;
-            }
-
-            positive_splits
-        }
     }
 
     /// A running race using the imperial scale, such as miles and yards.
@@ -470,6 +407,10 @@ pub mod running {
                 Some(p) => p,
                 None => Duration::new(0, 0)
             }
+        }
+
+        fn as_any(&self) -> &dyn Any {
+            self
         }
     }
 
@@ -543,6 +484,10 @@ pub mod running {
                 Some(p) => p,
                 None => Duration::new(0, 0)
             }
+        }
+
+        fn as_any(&self) -> &dyn Any {
+            self
         }
     }
 
@@ -620,19 +565,185 @@ pub mod running {
     }
 
     pub trait Running {
-        //fn new(race: dyn Race, runner: dyn Runner) -> Self;
+        fn new(duration: Duration) -> Self;
 
-        fn duration(&self) -> u64;
+        /// Returns the duration of the race.
+        fn duration(&self) -> Duration;
 
-        fn average_pace(&self) -> Duration;
+        /// Calculates the average pace based on distance and duration.
+        /// 
+        /// Examples:
+        /// 
+        /// ```
+        /// use std::time::Duration;
+        /// use librunner::running::{Race, ImperialRace, MetricRace};
+        /// 
+        /// let duration = Duration::new(14400, 0);
+        /// 
+        /// // Imperial marathon race. Average pace: 9:09/mile
+        /// let i_race: ImperialRace = Race::new(46112, duration);
+        /// assert_eq!(i_race.average_pace().as_secs(), 549);
+        /// assert_eq!(i_race.average_pace().as_secs() / 60, 9);
+        /// assert_eq!(i_race.average_pace().as_secs() % 60, 9);
+        /// 
+        /// // Metric marathon race. Average pace: 5:41/km
+        /// let m_race: MetricRace = Race::new(42195, duration);
+        /// assert_eq!(m_race.average_pace().as_secs(), 341);
+        /// assert_eq!(m_race.average_pace().as_secs() / 60, 5);
+        /// assert_eq!(m_race.average_pace().as_secs() % 60, 41);
+        /// ```
+        fn average_pace(&self, race: &impl Race) -> Duration;
 
-        fn speed(&self) -> f32;
+        fn speed(&self, race: &impl Race) -> f32;
 
-        fn splits(&self) -> Vec<Duration>;
+        fn num_splits(&self, race: &impl Race) -> u64;
 
-        fn negative_splits(&self) -> Vec<Duration>;
+        fn splits(&self, race: &impl Race) -> Vec<Duration>;
 
-        fn positive_splits(&self) -> Vec<Duration>;
+        /// Returns the splits of the race from a higher to a lower pace, according to the degree of variation.
+        /// 
+        /// # Arguments
+        /// 
+        /// * `degree` - the degree of variation from the average pace in seconds.
+        fn negative_splits(&self, race: &impl Race, degree: Duration) -> Vec<Duration> {
+            // minutes between minimal and maximum pace
+            let variation = (2 * degree.as_secs()) + 1;
+            let num_splits = self.num_splits(race);
+            // size of the block of splits with the same pace
+            let block = num_splits / variation;
+            let average_pace = self.average_pace(race);
+
+            let mut negative_splits = Vec::new();
+            // the pace starts high and decrements at every splits block
+            let mut pace = Duration::new(average_pace.as_secs() + degree.as_secs(), 0);
+            let mut block_count = 0;
+            
+            for _n in 0..num_splits as usize {
+                if block == block_count {
+                    // decrements the pace at every new block.
+                    let secs = pace.as_secs() - 1u64;
+                    pace = Duration::new(secs, 0);
+
+                    block_count = 0;
+                }
+                negative_splits.push(pace);
+                block_count += 1;
+            }
+
+            negative_splits
+        }
+
+        /// Returns the splits of the race from a lower to a higher pace, according to the degree of variation.
+        /// 
+        /// # Arguments
+        /// 
+        /// * `degree` - the degree of variation from the average pace in seconds.
+        fn positive_splits(&self, race: &impl Race, degree: Duration) -> Vec<Duration> {
+            let variation = (2 * degree.as_secs()) + 1;
+            let num_splits = self.num_splits(race);
+            // size of the block of splits with the same pace
+            let block = num_splits / variation;
+            let average_pace = self.average_pace(race);
+
+            let mut positive_splits = Vec::new();
+            // the pace starts high and decrements at every splits block
+            let mut pace = Duration::new(average_pace.as_secs() - degree.as_secs(), 0);
+            let mut block_count = 0;
+            
+            for _n in 0..num_splits as usize {
+                if block == block_count {
+                    // decrements the pace at every new block.
+                    let secs = pace.as_secs() + 1u64;
+                    pace = Duration::new(secs, 0);
+
+                    block_count = 0;
+                }
+                positive_splits.push(pace);
+                block_count += 1;
+            }
+
+            positive_splits
+        }
+
+        fn splits_with_pace(&self, race: &impl Race, pace: Duration) -> Vec<Duration> {
+            let mut splits = Vec::new();
+            
+            for _n in 0..self.num_splits(race) {
+                splits.push(pace);
+            }
+
+            splits
+        }
+    }
+
+    pub struct MetricRunning {
+        pub duration: Duration,
+    }
+
+    impl Running for MetricRunning {
+        fn new(duration: Duration) -> Self {
+            MetricRunning { 
+                duration,
+            }
+        }
+
+        fn duration(&self) -> Duration {
+            self.duration
+        }
+
+        fn average_pace(&self, race: &impl Race) -> Duration {
+            return Duration::new(
+                (MetricRace::SPLIT_DISTANCE as f32 * (self.duration().as_secs() as f32 / race.distance() as f32)
+            ) as u64, 0)
+        }
+
+        fn speed(&self, race: &impl Race) -> f32 {
+            race.distance() as f32 / self.duration().as_secs() as f32
+        }
+
+        fn num_splits(&self, race: &impl Race) -> u64 {
+            race.distance() / MetricRace::SPLIT_DISTANCE + if (race.distance() % MetricRace::SPLIT_DISTANCE) > 0 { 1 } else { 0 }
+        }
+
+        fn splits(&self, race: &impl Race) -> Vec<Duration> {
+            let average_pace = self.average_pace(race);
+            self.splits_with_pace(race, average_pace)
+        }
+    }
+
+    pub struct ImperialRunning {
+        pub duration: Duration,
+    }
+
+    impl Running for ImperialRunning {
+        fn new(duration: Duration) -> Self {
+            ImperialRunning { 
+                duration: duration,
+            }
+        }
+
+        fn duration(&self) -> Duration {
+            self.duration
+        }
+
+        fn average_pace(&self, race: &impl Race) -> Duration {
+            return Duration::new(
+                (ImperialRace::SPLIT_DISTANCE as f32 * (self.duration().as_secs() as f32 / race.distance() as f32)
+            ) as u64, 0)
+        }
+
+        fn speed(&self, race: &impl Race) -> f32 {
+            race.distance() as f32 / self.duration().as_secs() as f32
+        }
+
+        fn num_splits(&self, race: &impl Race) -> u64 {
+            race.distance() / ImperialRace::SPLIT_DISTANCE + if (race.distance() % ImperialRace::SPLIT_DISTANCE) > 0 { 1 } else { 0 }
+        }
+
+        fn splits(&self, race: &impl Race) -> Vec<Duration> {
+            let average_pace = self.average_pace(race);
+            self.splits_with_pace(race, average_pace)
+        }
     }
 }
 
@@ -644,6 +755,8 @@ mod tests {
     use crate::running::ImperialRace;
     use crate::running::MetricRace;
 
+    use crate::running::Running;
+    use crate::running::MetricRunning;
     use crate::utils::converter;
 
     #[test]
@@ -776,13 +889,14 @@ mod tests {
         let degree = Duration::new(5, 0);
         let variation = (2 * degree.as_secs()) + 1;
         let block = m_race.num_splits() / variation;
-        let negative_splits = m_race.negative_splits(degree);
+        let running: MetricRunning = Running::new(duration);
+        let negative_splits = running.negative_splits(&m_race, degree);
 
         assert_eq!(negative_splits[0].as_secs(), 346);
         assert_eq!(negative_splits[block as usize].as_secs(), 346 - 1);
         assert_eq!(negative_splits[block as usize * 2].as_secs(), 346 - 2);
         assert_eq!(negative_splits[block as usize * variation as usize].as_secs(), 346 - variation as u64);
-        assert_eq!(negative_splits[block as usize * degree.as_secs() as usize].as_secs(), m_race.average_pace().as_secs());
+        assert_eq!(negative_splits[block as usize * degree.as_secs() as usize].as_secs(), running.average_pace(&m_race).as_secs());
     }
 
     #[test]
@@ -793,12 +907,13 @@ mod tests {
         let degree = Duration::new(5, 0);
         let variation = (2 * degree.as_secs()) + 1;
         let block = m_race.num_splits() / variation;
-        let positive_splits = m_race.positive_splits(degree);
+        let running: MetricRunning = Running::new(duration);
+        let positive_splits = running.positive_splits(&m_race, degree);
 
         assert_eq!(positive_splits[0].as_secs(), 346 - (degree.as_secs() * 2) as u64);
         assert_eq!(positive_splits[block as usize].as_secs(), 346 - (degree.as_secs() * 2) as u64 + 1);
         assert_eq!(positive_splits[block as usize * 2].as_secs(), 346 - (degree.as_secs() * 2) as u64 + 2);
         assert_eq!(positive_splits[block as usize * variation as usize].as_secs(), 346 + 1);
-        assert_eq!(positive_splits[block as usize * degree.as_secs() as usize].as_secs(), m_race.average_pace().as_secs());
+        assert_eq!(positive_splits[block as usize * degree.as_secs() as usize].as_secs(), running.average_pace(&m_race).as_secs());
     }
 }
